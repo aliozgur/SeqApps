019af0eb6ed00e89600ba0bb72a6ceb702658400
MattMofDoom <matt.marlor@outlook.com>
2021-07-28 10:42:10 +1000

    Revert "Allow adding time tracking (initial time estimate, remaining time) and due date as static or dynamic properties"
    This reverts commit 7367f16224e8a8e1e03bc2aa3012a70d87633cd9.

--- src/Seq.Apps/Seq.Apps.Tests/JiraIssueReactorTests.cs
+++ src/Seq.Apps/Seq.Apps.Tests/JiraIssueReactorTests.cs
@@ -26,23 +26,5 @@ namespace Seq.Apps.Tests
             var evt = new Event<LogEvents.LogEventData>("11",1,DateTime.Now, new LogEvents.LogEventData { Exception = "Sample exception", Level = LogEvents.LogEventLevel.Error, RenderedMessage = "Rendered Test Message"});
             await jr.OnAsync(evt);
         }
-
-        [TestMethod]
-        public void TestDates()
-        {
-            Console.WriteLine("Test Valid Date Expression 1 ...");
-            Assert.IsTrue(JiraIssueReactor.ValidDateExpression("1h 30m"));
-            Console.WriteLine("Test Valid Date Expression 2 ...");
-            Assert.IsTrue(JiraIssueReactor.ValidDateExpression("1d 1h 30m"));
-            Console.WriteLine("Test Valid Date Expression 3 ...");
-            Assert.IsFalse(JiraIssueReactor.ValidDateExpression("2021-02-31"));
-            Console.WriteLine("Test Valid Date 1 ...");
-            Assert.IsTrue(JiraIssueReactor.ValidDate("2021-02-31"));
-            Console.WriteLine("Test Valid Date 2 ...");
-            Assert.IsFalse(JiraIssueReactor.ValidDate("1h 30m"));
-            Console.WriteLine("Test Calculate Date 1d 24h 30m equals " + DateTime.Today.AddDays(1).AddHours(24).AddMinutes(30).ToString("yyyy-MM-dd"));
-            Console.WriteLine(JiraIssueReactor.CalculateDateExpression("1d 24h 30m"));
-            Assert.IsTrue(JiraIssueReactor.CalculateDateExpression("1d 24h 30m") == DateTime.Today.AddDays(1).AddHours(24).AddMinutes(30).ToString("yyyy-MM-dd"));
-        }
     }
 }
